fe73ddec849690d4f0dd96554a40e64d8efb8e09
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2023-02-07 21:41:34 +0700

    update path "/register"

--- app.js
+++ app.js
@@ -3,14 +3,54 @@ require('./config/database').connect()
 
 const express = require('express')
 const User = require('./model/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
 app.use(express.json())
 
 //Register
-app.post("/register", (req, res) => {
-    
+app.post("/register", async (req, res) => {
+    try {
+        const { first_name, last_name, email, password } = req.body
+        
+        if (!(email && password && first_name && last_name)) {
+            res.status(400).send("All input is required")
+        }
+        
+        const oldUser = await User.findOne({ email })
+
+        if (oldUser) {
+            return res.status(409).send("User already exist. Please login")
+        }
+
+        // Encrypt user password
+        encryptedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            first_name,
+            last_name ,
+            email: email.toLowerCase(),
+            password: encryptedPassword 
+        })
+
+        const token = jwt.sign(
+            { user_id: user._id, email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h"
+            }
+        )
+
+        // save token
+        user.token = token
+
+        res.status(201).json(user)
+
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 //Login
